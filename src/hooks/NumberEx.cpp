f61130ec6bb84942c8c9aac711375df52507dffa
Robin <imer@imer.cc>
2013-09-12 23:56:15 +0200

    Fixed NumberEx

--- src/hooks/NumberEx.cpp
+++ src/hooks/NumberEx.cpp
@@ -1,23 +1,18 @@
 #include "NumberEx.hpp"
 #include "../libm2/game/misc.hpp"
 using namespace Hooks;
-NumberEx::NumberEx():Hook::Hook(){}
+NumberEx::NumberEx():Hook::Hook() {}
 
-int NumberEx::hook(int from, int to, const char *file, int line){
+int NumberEx::hook(int from, int to, const char *file, int line) {
+    int to2 = to;
+    if ( from > to )  {
+        to = from;
+        from = to2;
+    }
 
-
-  int from2 = from;
-  int to2 = to;
-
-  if ( from > to )
-  {
-    to = from;
-    from = to;
-  }
-
-  unsigned int div = to + 1 - from;
-  if (div==0)
-	return 0;
-
-  return from + thecore_random() % div;
+    unsigned int div = to + 1 - from;
+    if (div==0){
+        return 0;
+    }
+    return from + thecore_random() % div;
 }
--- src/hooks/NumberEx.hpp
+++ src/hooks/NumberEx.hpp
@@ -1,16 +1,16 @@
-#ifndef _HOOKS_DOWAR_HPP
-#define _HOOKS_DOWAR_HPP
+#ifndef _HOOKS_NUMBEREX_HPP
+#define _HOOKS_NUMBEREX_HPP
 #include "../libm2/lib/hook.hpp"
 #include "../libm2/addr.hpp"
 #include "../myAddr.hpp"
 using namespace libm2;
-namespace Hooks{
-class NumberEx: public Hook<int (*)(int, int, const char *, int),(unsigned int)myAddr::misc::number_ex,NumberEx>{
-    public:
+namespace Hooks {
+class NumberEx: public Hook<int (*)(int, int, const char *, int),(unsigned int)myAddr::misc::number_ex,NumberEx> {
+public:
     NumberEx();
-    static void hook(int, int, const char *, int));
+    static int hook(int from, int to, const char * file, int line);
 };
 }
-#endif // _HOOKS_DOWAR_HPP
+#endif // _HOOKS_NUMBEREX_HPP
 
 
