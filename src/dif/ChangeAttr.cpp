6e04c68c70bfed1898c976f3553944c127e05cf3
Robin <imer@imer.cc>
2013-09-15 23:22:20 +0200

    Added 6/7 attr enable dif

--- src/dif/ChangeAttr.cpp
+++ src/dif/ChangeAttr.cpp
@@ -9,13 +9,10 @@ ChangeAttr::ChangeAttr(){
     // min change_attr time (eventflag) =0
     libm2::dif((void*)0x80A6C94,new char(0x1),1);
     SetTime(1);
-    // enable 6/7 attr change/add item, just disables it for isYmir
-    libm2::dif((void*)0x80A1896,new unsigned char[2]{0x65,0x28},2);
-    libm2::dif((void*)0x80A18A7,new unsigned char[2]{0x55,0x28},2);
-    libm2::dif((void*)0x80A18B7,new unsigned char[2]{0x45,0x28},2);
-    libm2::dif((void*)0x80A73BE,new unsigned char[2]{0x3E,0xCD},2);
-    libm2::dif((void*)0x80A73D1,new unsigned char[2]{0x28,0xCD},2);
-    libm2::dif((void*)0x80A73E1,new unsigned char[2]{0x1B,0xCD},2);
+    // enable 6/7 attr change/add item for europe
+    libm2::dif((void*)0x80A73BE,new unsigned char(0x1E),1);
+    libm2::dif((void*)0x80A1897,new unsigned char[2]{0x45,0x2A},2);
+
 }
 void ChangeAttr::SetTime(int time){
     (*(int*)myAddr::misc::change_attr_time)=time;
